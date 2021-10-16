4d9d775854f24cc953c92f469b0bbbb31ebad2c2
Cody Lillywhite <codylillyw@gmail.com>
2021-10-16 12:19:30 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 $(function () {
-        $(".carousel").carousel( { interval: 2000, pause: "false" } );
-        $("#carouselButton").click(function(){
-          if($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-          }
-          else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-          }
-        });
-      });
+  $(".carousel").carousel({ interval: 2000, pause: "false" });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
+  $("#loginButton").on("click", () => {
+    $("#loginModal").modal('show');
+  });
+  $("#reserveButton").on("click", () => {
+    $("#reserveModal").modal("show");
+  });
+});
